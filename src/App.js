abb301c537ae2e3246739a2b1e160fdc261e4999
yujiayinshi <yujiayinshi@gmail.com>
2016-11-30 21:50:13 +0800

    1.wilddog as backend
    2.use material ui to make it beautiful

--- src/App.js
+++ src/App.js
@@ -1,49 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import {List, ListItem} from 'material-ui/List';
+import Checkbox from 'material-ui/Checkbox';
+import TextField from 'material-ui/TextField';
+import wilddog from 'wilddog'
+import logo from './todo.png';
 import './App.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
+        injectTapEventPlugin();
         this.state = {
             value: '',
             todos: []
         };
-        this.store = [];
+        const config = {
+            syncURL: "https://ystodos.wilddogio.com/todos"
+        };
+        wilddog.initializeApp(config);
+        this.ref = wilddog.sync().ref();
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.delToDo = this.delToDo.bind(this);
         this.completeToDo = this.completeToDo.bind(this);
         this.updateToDo = this.updateToDo.bind(this);
         this.getToDos = this.getToDos.bind(this);
+        this.getToDos();
     }
 
     getToDos(complete) {
-        this.complete = complete;
-        if (typeof this.complete !== 'undefined') {
-            const todos = this.store.filter(function(item) {
-                return item.complete === complete
-            });
-            this.setState({todos: todos});
-        } else {
-            this.setState({todos: this.store});
-        }
+        this.ref.on('value', (snapshot) => {
+            let todos = snapshot.val();
+            if (typeof complete !== 'undefined') {
+                todos = {};
+                for (let i in snapshot.val()) {
+                    if (snapshot.val()[i].complete === complete) {
+                        todos[i] = snapshot.val()[i];
+                    }
+                }
+            }
+            this.setState({todos: todos})
+        })
     }
 
     handleSubmit(event) {
         if (event && event.keyCode === 13) {
             event.preventDefault();
-            const todos = this.state.todos;
-            todos.push({
-                id: new Date().getTime(),
-                name: event.target.value,
-                complete: false
-            });
-            this.setState({
-                value: '',
-                todos: todos
-            });
-            this.store = todos;
+            if (event.target.value && event.target.value.trim()) {
+                this.ref.push({
+                    name: event.target.value.trim(),
+                    complete: false,
+                    createTime: this.ref.ServerValue.TIMESTAMP
+                });
+                this.setState({value: ''});
+            }
         }
     }
 
@@ -51,87 +64,65 @@ class App extends Component {
         this.setState({value: event.target.value})
     }
 
-    delToDo(todo) {
-        const todos = this.state.todos;
-        if (todos.indexOf(todo) > -1) {
-            todos.splice(todos.indexOf(todo), 1);
-            this.setState(this.state);
-        }
-        if (typeof this.complete === 'undefined') {
-            this.store = todos;
-        }
+    delToDo(id) {
+        wilddog.sync().ref(id).set(null);
     }
 
-    completeToDo(todo) {
-        const todos = this.state.todos;
-        for (let i = 0, ii = todos.length; i < ii; i++) {
-            if (todos[i].id === todo.id) {
-                todos[i].complete = !todos[i].complete;
-                break;
-            }
-        }
-        this.setState(this.state);
-        if (typeof this.complete === 'undefined') {
-            this.store = todos;
-        } else {
-            this.getToDos(this.complete);
-        }
+    completeToDo(id, todo) {
+        todo.complete = !todo.complete;
+        todo.updateTime = this.ref.ServerValue.TIMESTAMP;
+        wilddog.sync().ref(id).update(todo);
     }
 
-    updateToDo(todo) {
-        const todos = this.state.todos;
-        for (let i = 0, ii = todos.length; i < ii; i++) {
-            if (todos[i].id === todo.id) {
-                todos[i] = todo;
-                break;
-            }
-        }
-        this.setState(this.state);
-        if (typeof this.complete === 'undefined') {
-            this.store = todos;
-        } else {
-            this.getToDos(this.complete);
-        }
+    updateToDo(id, todo) {
+        wilddog.sync().ref(id).update(todo);
     }
 
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
+            <MuiThemeProvider>
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
 
-                    <h2>Welcome to React</h2>
+                        <h2>What to Do ?</h2>
+                    </div>
+                    <p className="App-intro">
+                        Record what you want to do and make evey day wonderful !
+                    </p>
+                    <div>
+                        <form onSubmit={this.handleSubmit}>
+                            <TextField
+                                name="newTodo"
+                                hintText="what to do ?"
+                                value={this.state.value}
+                                onChange={this.handleChange}
+                                onKeyDown={this.handleSubmit}
+                            />
+                        </form>
+                    </div>
+                    <ToDoList todos={this.state.todos} delToDo={this.delToDo} completeToDo={this.completeToDo}
+                              updateToDo={this.updateToDo}/>
+                    <ToDoFooter count={Object.keys(this.state.todos).length} getToDos={this.getToDos}/>
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-
-                <div>
-                    START WITH REACT
-                </div>
-                <div>
-                    <form onSubmit={this.handleSubmit}>
-                        <input type="text" value={this.state.value} onChange={this.handleChange}
-                               onKeyDown={this.handleSubmit}/>
-                    </form>
-                </div>
-                <ToDoList todos={this.state.todos} delToDo={this.delToDo} completeToDo={this.completeToDo} updateToDo={this.updateToDo} />
-                <ToDoFooter count={this.state.todos.length} getToDos={this.getToDos} />
-            </div>
+            </MuiThemeProvider>
         );
     }
 }
 
 class ToDoList extends Component {
     render() {
+        const items = [];
+        for (let todo in this.props.todos) {
+            if (this.props.todos.hasOwnProperty(todo)) {
+                items.push(<ToDoItem key={todo} id={todo} todo={this.props.todos[todo]} delToDo={this.props.delToDo}
+                                     completeToDo={this.props.completeToDo} updateToDo={this.props.updateToDo}/>);
+            }
+        }
         return (
-            <ul>
-                {
-                    this.props.todos.map((todo, index) =>
-                        <ToDoItem key={todo.id} todo={todo} delToDo={this.props.delToDo} completeToDo={this.props.completeToDo} updateToDo={this.props.updateToDo} />
-                    )
-                }
-            </ul>
+            <List className="list-container">
+                {items}
+            </List>
         );
     }
 }
@@ -150,11 +141,11 @@ class ToDoItem extends Component {
     }
 
     handClick() {
-        this.props.delToDo(this.props.todo);
+        this.props.delToDo(this.props.id);
     }
 
     handleChange() {
-        this.props.completeToDo(this.props.todo);
+        this.props.completeToDo(this.props.id, this.props.todo);
     }
 
     handleDoubleClick() {
@@ -179,21 +170,40 @@ class ToDoItem extends Component {
 
     handleChange4Edit(event) {
         this.props.todo.name = event.target.value;
-        this.props.updateToDo(this.props.todo);
+        this.props.updateToDo(this.props.id, this.props.todo);
     }
 
     render() {
         return (
-            <li className="list-item">
-                <input type="checkbox" checked={this.props.todo.complete} value={this.props.todo.complete} onChange={this.handleChange} />
+            <ListItem
+                leftCheckbox={<Checkbox value={this.props.todo.complete} checked={this.props.todo.complete}
+                                        onCheck={this.handleChange}/>}
+                rightToggle={<RaisedButton label="x" secondary onClick={this.handClick} className="del-button"/>}
+            >
                 <span onDoubleClick={this.handleDoubleClick} onBlur={this.handleBlur}>
                     {this.state.editTodo ?
-                        (<input type="text" value={this.props.todo.name} onChange={this.handleChange4Edit} onKeyDown={this.handleChangeUpdate} autoFocus />) :
-                        (<span className={this.props.todo.complete ? 'complete-todo' : 'active-todo'}>{this.props.todo.name}</span>)
+                        (<TextField
+                            name="todoName"
+                            value={this.props.todo.name}
+                            onChange={this.handleChange4Edit}
+                            onKeyDown={this.handleChangeUpdate}
+                            autoFocus
+                        />) :
+                        (<span
+                            className={this.props.todo.complete ? 'complete-todo' : 'active-todo'}>{this.props.todo.name}</span>)
                     }
                 </span>
-                <button onClick={this.handClick} className="del-button">x</button>
-            </li>
+            </ListItem>
+            // <li className="list-item">
+            //     <input type="checkbox" checked={this.props.todo.complete} value={this.props.todo.complete} onChange={this.handleChange} />
+            //     <span onDoubleClick={this.handleDoubleClick} onBlur={this.handleBlur}>
+            //         {this.state.editTodo ?
+            //             (<input type="text" value={this.props.todo.name} onChange={this.handleChange4Edit} onKeyDown={this.handleChangeUpdate} autoFocus />) :
+            //             (<span className={this.props.todo.complete ? 'complete-todo' : 'active-todo'}>{this.props.todo.name}</span>)
+            //         }
+            //     </span>
+            //     <button onClick={this.handClick} className="del-button">x</button>
+            // </li>
         )
     }
 }
@@ -204,6 +214,7 @@ class ToDoFooter extends Component {
         this.getAllToDos = this.getAllToDos.bind(this);
         this.getActiveToDos = this.getActiveToDos.bind(this);
         this.getCompletedToDos = this.getCompletedToDos.bind(this);
+        this.active = 'all';
     }
 
     getAllToDos() {
@@ -222,12 +233,13 @@ class ToDoFooter extends Component {
     }
 
     render() {
+        const style = {margin: 12};
         return (
             <div>
                 共{this.props.count}条
-                <button onClick={this.getAllToDos} className={this.active === 'all' ? 'active-button' : 'button'}>All</button>
-                <button onClick={this.getActiveToDos} className={this.active === 'active' ? 'active-button' : 'button'}>Active</button>
-                <button onClick={this.getCompletedToDos} className={this.active === 'completed' ? 'active-button' : 'button'}>Completed</button>
+                <RaisedButton label="All" primary={this.active === 'all'} onClick={this.getAllToDos} style={style}/>
+                <RaisedButton label="Active" primary={this.active === 'active'} onClick={this.getActiveToDos} style={style}/>
+                <RaisedButton label="Completed" primary={this.active === 'completed'} onClick={this.getCompletedToDos} style={style}/>
             </div>
         )
     }
